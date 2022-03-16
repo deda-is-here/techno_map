3f0e9291d3ad1955b6d552a1e8e339cbafebf435
ded <lacevichlacevich@mail.ru>
2022-03-16 18:02:15 +0300

    Изменил базы данных

--- app/models.py
+++ app/models.py
@@ -10,13 +10,36 @@ class ClubInfo(Base):
     id = Column(Integer, primary_key=True)
     club_name = Column(String(120), index=True, unique=True)
     club_address = Column(String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return f"Club name - {self.club_name},\nClub address - {self.club_address},"
+
+
+class User(Base):
+
+    __tablename__ = "user info"
+
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(120), index=True, unique=True)
+    password_hash = Column(String(128))
+
+    def __repr__(self):
+        return f"Username - {self.user_name}"
+
+
+class ClubContactInfo(Base):
+
+    __tablename__ = "contact info"
+
+    id = Column(Integer, primary_key=True)
     contact_username = Column(String(120), index=True, unique=True)
     contact_email = Column(String(120), index=True, unique=True)
     contact_password_hash = Column(String(128))
 
     def __repr__(self):
-        return f"Club name - {self.club_name},\nClub address - {self.club_address}," \
-               f"\nContact email - {self.contact_email}"
+        f"""Contact username - {self.contact_username},\n
+        Contact email - {self.contact_email}
+        """
 
 
 if __name__ == "__main__":
