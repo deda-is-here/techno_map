dff06349d2a08f255f80e6ee9eb91701f55ebe87
ded <lacevichlacevich@mail.ru>
2022-03-19 08:59:55 +0300

    Часть кода ошибки

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'you-will-never-guess'
+    ENGINE_URL = os.getenv("ENGINE_URL")
